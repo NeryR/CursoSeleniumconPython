707a342c6a5347342ecbd51b14dabeb51b043240
neryr <nery.reyesrt@hotmail.com>
2022-06-15 16:40:07 -0500

    Primera clase

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+
+#Esta línea sirve para preparar el entorno de prueba
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path=r'/home/neryr/chromedriver/stable/chromedriver')
+        driver = cls.driver
+        driver.implicitly_wait(10)
+
+#Entorno de prueba y acciones a realizar en este entorno
+    def test_hello_world(cls):
+        driver = cls.driver
+        driver.get('https://www.platzi.com')
+
+    def test_visit_wikipedia(cls):
+        cls.driver.get('https://www.wikipedia.org')
+
+#Acciones finales a realizar, en este caso sería cerrar las ventanas al finalizar
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reportes', report_name='hello-world-report'))
